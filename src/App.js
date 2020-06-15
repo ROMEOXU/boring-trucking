b52967a94ac95bb319848dd47f9d877a6b070b9e
Mac <xuromeo@gmail.com>
2020-06-14 19:03:53 -0700

    update

--- src/App.js
+++ src/App.js
@@ -7,17 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Welcome to Boring trucking</h1>
+       <h3>we provide high quality Port Drayage service</h3>
+       
       </header>
     </div>
   );
